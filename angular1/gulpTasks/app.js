18024348d669b5e390463b743435a8d7205ff627
lbtrier <leonardobatri@gmail.com>
2017-03-22 22:28:15 -0300

    Build configuration using Gulp

---
+++ angular1/gulpTasks/app.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const uglifycss = require('gulp-uglifycss');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets']);
+
+gulp.task('app.html', function() {
+    gulp.src('app/**/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('public'));
+})
+
+gulp.task('app.css', function() {
+    gulp.src('app/**/*.css')
+        .pipe(uglifycss({
+            "uglycomments": true
+        }))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('public/assets/css'));
+})
+
+gulp.task('app.js', function() {
+    gulp.src('app/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('public/assets/js'));
+})
+
+gulp.task('app.assets', function() {
+    gulp.src('assets/**/*.*')
+        .pipe(gulp.dest('public/assets'));
+})
